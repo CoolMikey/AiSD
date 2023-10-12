acaabddb1b71dc10e8b2a8f380b4bb6384597fae
CoolMikey <mikey@matuszyk.com>
2023-10-12 13:09:22 +0200

    Added a few functions to demonstrate how a simple list works.

--- demonstrator.py
+++ demonstrator.py
@@ -3,8 +3,36 @@ By Michał Matuszyk
 on 12/10/2023
 """
 from simpleElement import simpleElement
+from lista_jednokierunkowa import listaJednokierunkowa
 
 
-elementy = []
-for i in range(10):
-    elementy.append(simpleElement(i))
+
+def createASimpleList(n): #create a simple list with n elements, only one-way (next)
+    if n < 0:
+        raise Exception("Number of elements can't be less than 0")
+    if n == 1:
+        return listaJednokierunkowa()
+    # n > 1
+    head = simpleElement(0)
+    current_elem = head
+    for i in range(n-1):
+        current_elem.next = simpleElement(i+1)
+        current_elem = current_elem.next
+    wynik = listaJednokierunkowa()
+    wynik.head = head
+    return wynik
+
+
+def demonstrate_simple_list():
+    lista_1 = createASimpleList(10) # creates a simple 10 element list
+    print("Liczba elem. listy:", len(lista_1))
+    #### Add an element
+    new_element = simpleElement("Nowa wartosc xyz")
+    lista_1.addElementToEnd(new_element)
+    print("Liczba elem. listy po dodaniu:", len(lista_1))
+    lista_1.removeLastElement()
+    print("Liczba elem. listy po usunieciu:", len(lista_1))
+
+
+if __name__ == "__main__":
+    demonstrate_simple_list()
--- lista_jednokierunkowa.py
+++ lista_jednokierunkowa.py
@@ -11,6 +11,30 @@ class listaJednokierunkowa:
         if self.head == None:
             head = newElement
         current_element = self.head
-        while self.head.next != None:
+        while current_element.next != None:
             current_element = current_element.next
         current_element.next = newElement
+
+    def removeLastElement(self):
+        if self.head == None: # if list is empty - do nothing
+            return
+        if self.head.next == None: # if only 1 elem. in list - remove the head
+            self.head = None
+        # Now we know, that the list contains a head and a next elem.
+        current_element = self.head.next
+        previous_elem = self.head
+        while current_element.next != None:
+            previous_elem = current_element
+            current_element = current_element.next
+        previous_elem.next = None
+
+
+    def __len__(self):
+        if self.head == None:
+            return 0
+        i = 1
+        currentElement = self.head
+        while currentElement.next != None:
+            currentElement = currentElement.next
+            i += 1
+        return i
